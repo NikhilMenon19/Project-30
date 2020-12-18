9c6f1add85bca55169145e94dbcabe8bf1ee9ffc
Asma2442 <70644511+Asma2442@users.noreply.github.com>
2020-12-18 22:04:33 +0530

    Update Block.js

--- js/Block.js
+++ js/Block.js
@@ -29,6 +29,9 @@ class Block{
         World.remove(world, this.body);
         push();
         this.visibility = this.visibility -5;
+        tint(255,this.visibility)
+        rectMode(CENTER);
+        rect(0,0,this.width, this.height);
         pop();
         
       }
